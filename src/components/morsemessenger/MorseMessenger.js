be2302aaade43ed5edeb292bf3403431c4931456
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2021-02-07 15:15:44 -0500

    morse messenger dark mode

--- src/components/morsemessenger/MorseMessenger.js
+++ src/components/morsemessenger/MorseMessenger.js
@@ -39,6 +39,12 @@ const useStyles = theme =>({
         minHeight:"250px",
         maxHeight:"100%"
     },
+    creditsGrid:{
+        backgroundColor:'white',
+    },
+    darkCreditsGrid:{
+        backgroundColor:"rgb(24, 26, 27)",
+    },
     oval:{
         textAlign:"center",
         verticalAlign:"middle",
@@ -46,6 +52,15 @@ const useStyles = theme =>({
         width:"290px",
         height:"75px",
         backgroundColor:"#d4ebf2",
+        borderRadius:"40px"
+    },
+    darkOval:{
+        textAlign:"center",
+        verticalAlign:"middle",
+        marginTop:"20px",
+        width:"290px",
+        height:"75px",
+        backgroundColor:"rgb(20, 54, 65)",
         borderRadius:"40px"
     },
     'tryitout:visited':{
@@ -107,6 +122,12 @@ const useStyles = theme =>({
     backdrop: {
         zIndex: theme.zIndex.drawer + 1,
         color: '#fff',
+    },
+    darkText:{
+        color:"white"
+    },
+    darkTextField:{
+        backgroundColor:"white"
     }
 });
 
@@ -133,6 +154,12 @@ class MorseMessenger extends Component {
   render(){
       const {classes} = this.props;
       const {error, response_message, message_sent} = this.state; 
+      let darkText = {
+          color:"white"
+      }
+      let lightText={
+          color:"black"
+      }
     return(
       <React.Fragment>
           <Header
@@ -141,26 +168,26 @@ class MorseMessenger extends Component {
             <Grid className={this.state.nightModeChecked?classes.darkFirstGrid:classes.firstGrid} justify="center" alignItems="center" container>
                 <Grid item xs={12} sm={6} md={4} align="center">
                     <Box style={{ width:"50%"}}>
-                        <h1 className={classes.titleText}>Morse</h1>
-                        <h1 className={classes.titleText}>Messenger</h1>
+                        <h1 className={classes.titleText} style={this.state.nightModeChecked?darkText:lightText}>Morse</h1>
+                        <h1 className={classes.titleText} style={this.state.nightModeChecked?darkText:lightText}>Messenger</h1>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4} align="center">
                     <a href="#tryitout" className={classes.tryitout}>
-                        <Box className={classes.oval}>
-                            <h1 className={classes.buttonCenteredText}>Try Now</h1>
+                        <Box className={this.state.nightModeChecked?classes.darkOval:classes.oval}>
+                            <h1 className={classes.buttonCenteredText} style={this.state.nightModeChecked?darkText:lightText}>Try Now</h1>
                         </Box>
                     </a>
-                    <p className={classes.titleExplainer} >Morse Messenger is a revolutionary application which truly tests the limits of online communication by removing the need see and hear</p>
+                    <p className={classes.titleExplainer} style={this.state.nightModeChecked?darkText:lightText} >Morse Messenger is a revolutionary application which truly tests the limits of online communication by removing the need see and hear</p>
                 </Grid>
             </Grid>
-            <Grid className={classes.secondGrid} justify="center" alignItems="center" container>
+            <Grid className={this.state.nightModeChecked?classes.darkSecondGrid:classes.secondGrid} justify="center" alignItems="center" container>
                 <Grid item xs={12} sm={4} md={3} align="center">
                     <Box style={{display:"flex"}}>
                         <WifiIcon style={{fontSize:"125px"}}/>                        
                         <Box>
-                            <h1 style={{textAlign:"left"}}>Scalable</h1>
-                            <h4 style={{textAlign:"left"}}>Working on Pusher infrastructure, if I paid more money, this would rival Facebook Messenger</h4>
+                            <h1 style={{textAlign:"left"}} style={this.state.nightModeChecked?darkText:lightText}>Scalable</h1>
+                            <h4 style={{textAlign:"left"}} style={this.state.nightModeChecked?darkText:lightText}>Working on Pusher infrastructure, if I paid more money, this would rival Facebook Messenger</h4>
                         </Box>
                     </Box>
                 </Grid>
@@ -168,8 +195,8 @@ class MorseMessenger extends Component {
                     <Box style={{display:"flex"}}>                            
                         <FlashOnIcon style={{fontSize:"125px"}}/>
                         <Box>
-                            <h1 style={{textAlign:"left"}}>Hassle Free</h1>
-                            <h4 style={{textAlign:"left"}}>No extra bells and whistles to learn. The only requirement is an expert proficiency in morse code</h4>
+                            <h1 style={{textAlign:"left"}} style={this.state.nightModeChecked?darkText:lightText}>Hassle Free</h1>
+                            <h4 style={{textAlign:"left"}} style={this.state.nightModeChecked?darkText:lightText}>No extra bells and whistles to learn. The only requirement is an expert proficiency in morse code</h4>
                         </Box>
                     </Box>
                 </Grid>
@@ -177,23 +204,23 @@ class MorseMessenger extends Component {
                     <Box style={{display:"flex"}}>                            
                         <LockIcon style={{fontSize:"125px"}}/>
                         <Box>
-                            <h1 style={{textAlign:"left"}}>Secure</h1>
-                            <h4 style={{textAlign:"left"}}>I will never sell your secrets to the NSA simply because I don't have to infrastructure to keep them</h4>
+                            <h1 style={{textAlign:"left"}} style={this.state.nightModeChecked?darkText:lightText}>Secure</h1>
+                            <h4 style={{textAlign:"left"}} style={this.state.nightModeChecked?darkText:lightText}>I will never sell your secrets to the NSA simply because I don't have to infrastructure to keep them</h4>
                         </Box>
                     </Box>
                 </Grid>
             </Grid>
             <Grid className={this.state.nightModeChecked?classes.darkFirstGrid:classes.firstGrid} justify="center" alignItems="center" container>
                 <Grid item xs={12} align="center">
-                    <h1 style={{fontSize:"75px"}}>How Does It Work?</h1>
+                    <h1 style={{fontSize:"75px"}} style={this.state.nightModeChecked?darkText:lightText}>How Does It Work?</h1>
                     <Divider style={{width:"50%"}}variant="middle"/>
                 </Grid>
             </Grid>
-            <Grid className={classes.secondGrid} justify="center" alignItems="center" container>
+            <Grid className={this.state.nightModeChecked?classes.darkSecondGrid:classes.secondGrid} justify="center" alignItems="center" container>
                     <Grid item xs={6} sm={5} md={4} align="center">
                         <Box className={classes.stepBox}>
-                            <h1 className={classes.stepNumber}>1</h1>
-                            <p className={classes.stepInstructionLeft}>Download the Morse Messenger app on the Apple App store.</p>
+                            <h1 className={classes.stepNumber} style={this.state.nightModeChecked?darkText:lightText}>1</h1>
+                            <p className={classes.stepInstructionLeft} style={this.state.nightModeChecked?darkText:lightText}>Download the Morse Messenger app on the Apple App store.</p>
                         </Box>
                     </Grid>
                     <Grid item xs={6} sm={5} md={4} align="center">
@@ -206,16 +233,16 @@ class MorseMessenger extends Component {
                 </Grid>
                 <Grid item xs={6} sm={5} md={4} align="center">
                     <Box className={classes.stepBox}>
-                        <p className={classes.stepInstructionRight}>Join a Channel on the app.</p>
-                        <h1 className={classes.stepNumber}>2</h1>
+                        <p className={classes.stepInstructionRight} style={this.state.nightModeChecked?darkText:lightText}>Join a Channel on the app.</p>
+                        <h1 className={classes.stepNumber} style={this.state.nightModeChecked?darkText:lightText}>2</h1>
                     </Box>
                 </Grid>
             </Grid>
-            <Grid className={classes.secondGrid} justify="center" alignItems="center" container>
+            <Grid className={this.state.nightModeChecked?classes.darkSecondGrid:classes.secondGrid} justify="center" alignItems="center" container>
                 <Grid item xs={6} sm={5} md={4} align="center">
                     <Box className={classes.stepBox}>
-                        <h1 className={classes.stepNumber}>3</h1>
-                        <p className={classes.stepInstructionLeft}>Send a message on this website with the same channel</p>
+                        <h1 className={classes.stepNumber} style={this.state.nightModeChecked?darkText:lightText}>3</h1>
+                        <p className={classes.stepInstructionLeft} style={this.state.nightModeChecked?darkText:lightText}>Send a message on this website with the same channel</p>
                     </Box>
                 </Grid>
                 <Grid item xs={6} sm={5} md={4} align="center">
@@ -234,22 +261,22 @@ class MorseMessenger extends Component {
                             <Box style={{display:"flex"}} m={2}>
                                 <Grid alignItems="center" justify="center" container>
                                     <Grid item xs={6} sm={5} md={4}>
-                                        <TextField required id="channelInput" label="Channel" variant="filled" />
+                                        <TextField className={this.state.nightModeChecked?classes.darkTextField:""} required id="channelInput" label="Channel" variant="filled" />
                                     </Grid>
                                     <Grid item xs={6} sm={5} md={4}>
-                                         <TextField required id="identifierInput" label="Identifier" variant="filled" />
+                                         <TextField className={this.state.nightModeChecked?classes.darkTextField:""} required id="identifierInput" label="Identifier" variant="filled" />
                                     </Grid>
                                 </Grid>
                             </Box>
-                            <TextField style={{width:"50%"}}required id="messageInput" label="Message" variant="filled" fullWidth />
+                            <TextField className={this.state.nightModeChecked?classes.darkTextField:""} style={{width:"50%"}}required id="messageInput" label="Message" variant="filled" fullWidth />
                             <Button style={{display:"block", marginTop:"10px"}}type="submit" variant="contained" color="primary" endIcon={<Icon>send</Icon>}>Send a Morse Message</Button>
                         </form>
                     </fieldset>
                 </Grid>
             </Grid>
-            <Grid alignItems="center" justify="center" container>
+            <Grid className={this.state.nightModeChecked?classes.darkCreditsGrid:classes.creditsGrid} alignItems="center" justify="center" container>
                 <Grid item xs={12} align="center">
-                    <h4>Design By James Haggarty</h4>
+                    <h4 style={this.state.nightModeChecked?darkText:lightText}>Design By James Haggarty</h4>
                 </Grid>
             </Grid>
             <Backdrop className={classes.backdrop} open={this.state.isLoading}>
