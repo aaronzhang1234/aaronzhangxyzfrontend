9a5ab5d18da93af004f0d1810ca9bfedc505f395
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2021-02-13 19:22:42 -0500

    results style changes

--- src/components/fomo/Results.js
+++ src/components/fomo/Results.js
@@ -158,16 +158,19 @@ class Results extends Component{
             let amt_string = amt_shares.toLocaleString();
             let total_money = Math.floor((amt_shares * to_price)*100)/100; 
             let total_money_formatted = accounting.formatMoney(total_money);
-            let money_gained = accounting.formatMoney(Math.abs(total_money - starting_money));
+            let money_gained = accounting.formatMoney(total_money - starting_money);
+            let textStyle={
+                fontSize:"30"
+            }
             return(
                 <div id="total-results">
+                     <h1 style={{...textColor,...{fontSize:"50px", marginBottom:"0", marginTop:"5px"}}}>You would have:</h1>
                      <Box justifyContent="center" alignItems="center" style={{display:"flex"}}>
                         <img className="reaction-images" src={reaction_image}/> 
                         <div style={{margin:"30px"}}>
-                            <h1 style={textColor}> You would have: </h1>
-                            <h1 style={textColor}>{amt_string} share(s) worth {total_money_formatted}</h1>
-                            <h1 style={textColor}>percentage {gain_loss} of {percentage_string}% </h1> 
-                            <h1 style={textColor}>and a monetary {gain_loss} of {money_gained}</h1>
+                            <h1 style={{...textColor,...{fontSize:"40px"}}}>{amt_string} share(s) worth {total_money_formatted}</h1>
+                            <h1 style={textColor}>Percentage {gain_loss} : {percentage_string}% </h1> 
+                            <h1 style={textColor}>Monetary {gain_loss} : {money_gained}</h1>
                         </div>
                         <img className="reaction-images" src={reaction_image}/>
                      </Box>
